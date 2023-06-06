787ac4e4add3bb4f4a86e99ee64f3739b4848005
kalkidanh <kalkidanh89@gmail.com>
2023-06-06 21:54:22 +0300

    sorted keywords printed

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""A function that uses recursion to query the Reddit API, parses the title of
+all hot articles, and returns a case-sensitive count of given keywords."""
+import requests
+
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Return the number of words."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {('User-Agent'): ('chrome 51.0.2704.106')}
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
